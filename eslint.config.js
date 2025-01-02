c6c32d89edf1dd85b51edc657df63d0c7c03152e
Rania Chouchene <rania.chouchene.2019@gmail.com>
2025-01-02 21:14:02 +0100

    changing lint conf

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,25 @@
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-
+import * as pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginReact.configs.flat.recommended,
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  {
+    files: ["**/src/Interfaces/*.tsx"],
+    ignores: ["**/node_modules/", "**/src/tests/"],
+  },
+  {
+    plugins: {
+      react: pluginReact,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
 ];
